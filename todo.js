787bc4b49927b974aafd5a61bbb73d44bcf49fd1
AnastasiaGWolf <solokhina.g.wolf@gmail.com>
2024-03-05 11:18:46 +0400

    Initialization project

---
+++ todo.js
@@ -0,0 +1,14 @@
+//! 1. Инициализация проекта:
+
+// * npm init -y
+// * npx create-gitignore node
+// * npx eslint --init
+// * npm i sequelize sequelize-cli pg pg-hstore
+// * npm i express
+// * npm i -D nodemon morgan
+// * npm install @babel/core @babel/preset-env @babel/preset-react @babel/register react react-dom
+// * npm i dotenv
+// * npm i express-session session-file-store bcrypt
+// * npm i ws
+
+// * npx sequelize init
